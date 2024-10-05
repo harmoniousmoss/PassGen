296119b8fae57699befb65e4ce334a745d3de64c
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-05 18:35:10 +0700

    Initial development

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+mod views;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(views::index)
+            .service(fs::Files::new("/static", "./static").show_files_listing())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
