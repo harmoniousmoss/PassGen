a0c6dfbc5e44e0ea5ba1d5c7080fcd1db9a76239
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-05 18:47:37 +0700

    Move generate password to Rust backend

--- src/views.rs
+++ src/views.rs
@@ -1,7 +1,23 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder}; // Add `web` module here
+use rand::{distributions::Alphanumeric, Rng};
+use serde_json::json;
 
+// The homepage rendering function
 #[get("/")]
 pub async fn index() -> impl Responder {
     let html = include_str!("../templates/index.html");
     HttpResponse::Ok().content_type("text/html").body(html)
+}
+
+// The password generation endpoint
+#[get("/generate-password/{length}")]
+pub async fn generate_password(length: web::Path<usize>) -> impl Responder {
+    let password: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(*length) // The password length from the path parameter
+        .map(char::from)
+        .collect();
+
+    let response = json!({ "password": password });
+    HttpResponse::Ok().json(response) // Return JSON response
 }
